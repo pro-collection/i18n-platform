4a98d26a3ff793629ce40c61f116629ddad4882d
yanlele <331393627@qq.com>
2020-08-08 19:46:11 +0800

    feat: 添加mybatis plus 配置

---
+++ src/main/java/com/yanle/i18nplatform/config/MybatisPlusConfig.java
@@ -0,0 +1,16 @@
+package com.yanle.i18nplatform.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com/yanle/i18nplatform/mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}

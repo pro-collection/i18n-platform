e56daabedcd5ff6cab6ce414dcc80acba0e65717
yanlele <331393627@qq.com>
2020-08-08 14:34:41 +0800

    feat: init

---
+++ src/main/java/com/yanle/i18nplatform/I18nPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yanle.i18nplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18nPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I18nPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yanle/i18nplatform/I18nPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanle.i18nplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

68d11de11c8193760ebf70c9a9c7ae479f67c9ba
yanlele <331393627@qq.com>
2020-08-15 21:38:23 +0800

    feat: 添加test

--- src/main/java/com/yanle/i18nplatform/controller/ProjectController.java
+++ src/main/java/com/yanle/i18nplatform/controller/ProjectController.java
@@ -1,21 +1,25 @@
 package com.yanle.i18nplatform.controller;
 
 
+import com.yanle.i18nplatform.common.base.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author yanle
  * @since 2020-08-08
  */
 @RestController
-@RequestMapping("/project")
+@RequestMapping("/api/project")
 public class ProjectController {
-
+    @GetMapping
+    public ApiResponse add() {
+        return ApiResponse.success("返回成功");
+    }
 }
 
